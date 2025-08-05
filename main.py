7987a210b6ad7f944964f5aa50f628b4b717779a
Kaysar Md Nadim <39157267+nadimkaysar@users.noreply.github.com>
2025-08-05 18:33:54 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from time import sleep
+from navigation import make_sidebar
+from st_pages import hide_pages
+from sqlalchemy import text
+from streamlit_js_eval import streamlit_js_eval
+
+#streamlit_js_eval(js_expressions="parent.window.location.reload()")
+make_sidebar()
+
+st.title("Mental Health Support System")
+
+
+username = st.text_input("Email")
+password = st.text_input("Password", type="password")
+hide_pages(["chat","main","registration"])
+
+connection_url = (
+    'postgresql://neondb_owner:npg_VX32wptUvbBr@ep-broad-bush-aeswvk7d-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+)
+
+# Initialize connection with kwargs
+conn = st.connection("neondb", type="sql", url=connection_url)
+
+if st.button("Log in", type="primary"):
+    if username and password:
+        with conn.session as session:
+            result = session.execute(
+                text("SELECT id, email FROM users WHERE email = :email AND password = :password"),
+                {"email": username, "password": password}
+            ).fetchone()
+
+        if result:
+            user_id, user_email = result
+            #cookie_controller.set("UserId",id)
+            st.session_state.logged_in = True
+            st.session_state['Email'] = user_email
+            st.session_state['id'] = user_id
+            st.success("Logged in successfully!")
+            sleep(0.5)
+            print("Session State")
+
+            print(st.session_state['id'])
+            #print(cookie_controller.get("UserId"))
+            print(st.session_state['Email'])
+            st.switch_page("pages/chat.py")
+        else:
+            st.error("Incorrect username or password")
+    else:
+        st.warning("Please enter both username and password.")
